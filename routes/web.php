531cd07f66b13d15ffdb5a4dedd127bdbb9b18e0
Awan A <aoneholics@gmail.com>
2022-07-23 00:47:41 +0700

    Update Materi 13 - Search & Pagination

--- routes/web.php
+++ routes/web.php
@@ -37,17 +37,18 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[
-        'title' => "Post By Category : $category->name",
-        'posts' => $category->posts->load('category','user'),
-    ]);
-});
 
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category','user'),
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts',[
+//         'title' => "Post By Category : $category->name",
+//         'posts' => $category->posts->load('category','user'),
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function(User $author){
+//     return view('posts',[
+//         'title' => "Post By Author : $author->name",
+//         'posts' => $author->posts->load('category','user'),
+//     ]);
+// });
 
