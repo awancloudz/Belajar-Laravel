21516f400d61b4a4560f91ca2a2b0f79dfd4e19d
Awan A <aoneholics@gmail.com>
2022-07-24 01:39:39 +0700

    Update Materi 14 - View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\User;
-use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,6 +38,8 @@ Route::get('/categories', function(){
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view('posts',[
 //         'title' => "Post By Category : $category->name",
