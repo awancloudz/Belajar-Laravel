281259aad1c07e173cd83b90ea611c5fe53aa331
Awan A <aoneholics@gmail.com>
2022-07-25 02:26:29 +0700

    Update Materi 16 - Userlogin & middleware

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -19,11 +19,30 @@
                 <a class="nav-link {{ ($title === "Post Categories") ? 'active' : '' }}" href="/categories">Categories</a>
                 </li>
         </ul>
+
         <ul class="navbar-nav ms-auto">
+            @auth
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Welcome back, {{ auth()->user()->name }}
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i> Logout</button>
+                    </form>
+                    {{-- <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-in-right"></i> Logout</a></li> --}}
+                </ul>
+            </li>
+            @else
             <li class="nav-item">
                 <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
             </li>
+            @endauth
         </ul>
+        
         </div>
     </div>
 </nav>
