42ba7f3e82d779dcf2e7bacb911861f5ad545cda
Awan A <aoneholics@gmail.com>
2022-07-28 00:44:19 +0700

    Update Materi 23 - Authorization

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,5 +14,21 @@
           </a>
         </li>
       </ul>
+
+      {{-- Pemakaian Gate (authorization)--}}
+      @can('admin')
+      <h6 class="sidebar-headeing d-flex justify-content-beetwen align-item-center px-3 mt-4 mb-1 text-muted">
+        <span>Administrator</span>
+      </h6>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" aria-current="page" href="/dashboard/categories">
+            <span data-feather="grid" class="align-text-bottom"></span>
+            Post Categories
+          </a>
+        </li>
+      </ul>
+      @endcan
+
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -52,6 +53,10 @@ Route::get('/dashboard', function(){
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug']);
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+//Jika  Pakai Middleware IsAdmin & Kernel
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
+
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view('posts',[
